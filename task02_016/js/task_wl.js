69c6046a569893b2b20ba3b560cd96096dbf636d
wanglei-0707 <806935949@qq.com>
2016-03-29 11:02:44 +0800

    task16

--- task02_016/js/task_wl.js
+++ task02_016/js/task_wl.js
@@ -30,6 +30,8 @@ function addAqiData() {
 		return;
 	}
 	aqiData[city] = aq;
+	document.getElementById("aqi-city-input").value = "";
+	document.getElementById("aqi-value-input").value = "";
 }
 
 /**
@@ -39,14 +41,9 @@ function renderAqiList() {
 	var header = "<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>"
 	var tr = ""
 	for(var city in aqiData){
-		tr += "<tr><td>" + city + "</td><td>" + aqiData[city] +"</td><td><button>删除</button></td></tr>";
+		tr += "<tr><td>" + city + "</td><td>" + aqiData[city] +"</td><td><button data-city='"+city+"'>删除</button></td></tr>";
 	}
-	if(tr==""){
-		return
-	}
-	else{
-		aqi_table.innerHTML = header+tr;
-	}
+	aqi_table.innerHTML = header+tr;
 }
 
 /**
@@ -64,7 +61,7 @@ function addBtnHandle() {
  */
 function delBtnHandle(ct) {
   // do sth.
-  delete aqiData[ct]
+  delete aqiData[ct];
   renderAqiList();
 }
 
@@ -84,20 +81,23 @@ function addEvent(obj,type,fn){
 	}
 }
 
-function deleteBtnEvent(target,){
-
-
+function deleteBtnEvent(obj,tag,type,fn){
+	addEvent(obj,type,function(event){
+		var target = getEventTarget(event);
+		if(target && target.tagName === tag.toUpperCase()){
+			fn(event.target.dataset.city);
+			//fn.call(null, event.target.dataset.city);
+		}
+	});
 }
 
 function init() {
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
   var button = document.getElementById("add-btn");
-  button.onclick = function(){
-  	addBtnHandle();
-  }
+  addEvent(button,"click",addBtnHandle)
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-  
+  deleteBtnEvent(aqi_table, "button", "click", delBtnHandle);
 }
 
 init();
