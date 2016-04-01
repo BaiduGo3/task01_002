f0741c0a443f4231365f45b78eee8e7b1f3282f2
wanglei-0707 <806935949@qq.com>
2016-04-01 20:26:08 +0800

    task18

--- task02_018/js/wl.js
+++ task02_018/js/wl.js
@@ -23,8 +23,10 @@ function addEvent(ele,type,handler){
 
 //获取输入的值
 function getValue(){
-	var number = $("inputNum").value;
-	if(!/^\d+$/.test(number)){
+	var number = $("inputNum").value.trim();
+	if(!number){
+		alert("输入不能为空");
+	}else if(!/^\d+$/.test(number)){
 		alert("输入的值必须是数字！");
 	}else{
 		return number;
@@ -39,54 +41,59 @@ function clearValue(){
 //左侧入按钮点击事件绑定
 addEvent(btnLeftIn,"click",function(){
 	var number = getValue();
-	queue.unshift(number);
-	renderQueue();
-	clearValue();
+	if(number){
+		queue.unshift(number);
+		renderQueue();
+		clearValue();
+	}
 })
 
 //右侧入按钮点击事件绑定
 addEvent(btnRightIn,"click",function(){
 	var number = getValue();
-	queue.push(number);
-	renderQueue();
-	clearValue();
+	if(number){
+		queue.push(number);
+		renderQueue();
+		clearValue();
+	}
+	
 })
+
+//移出元素
+function removeNum(num){
+	if(num){
+		alert(num);
+		renderQueue();
+		clearValue();
+	}else{
+		alert("无元素可移出！");
+	}
+}
 
 //左侧出按钮事件绑定
 addEvent(btnLeftOut,"click",function(){
 	var deleteNum = queue.shift();
-	alert(deleteNum);
-	renderQueue();
-	clearValue();
+	removeNum(deleteNum);
 })
 
 //右侧出按钮事件绑定
 addEvent(btnRightOut,"click",function(){
 	var deleteNum = queue.pop();
-	alert(deleteNum);
-	renderQueue();
-	clearValue();
+	removeNum(deleteNum);
 })
 
-//删除元素
-function deleteNum(index){
-	if(index==queue.length){
-		queue.pop();
-	}else{
-		var arry1 = queue.slice(0,index);
-		var arry2 = queue.slice(parseInt(index)+1);
-		queue = arry1.concat(arry2);
-	}
+//删除队列中元素
+function deleteNum(index,num){
+	queue.splice(index,1);
 	renderQueue()
 }
-
 
 function delegateEvent(ele,type,tag,handler){
 	addEvent(ele,type,function(event){
 		event = event || window.event;
 		var target = event.target || event.srcElement;
 		if(target && target.tagName === tag.toUpperCase()){
-			handler.call(this,target.id);
+			handler.call(this,target.id,target.innerHTML);
 		}
 	});
 }
