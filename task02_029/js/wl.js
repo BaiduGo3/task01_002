86a203fef10fd7f9733d68625f3c5301d37a1f96
wanglei-0707 <806935949@qq.com>
2016-04-12 17:35:04 +0800

    task30

--- task02_029/js/wl.js
+++ task02_029/js/wl.js
@@ -1,4 +1,5 @@
-var	test = document.getElementById('test'),
+var	nameinput = document.getElementById('name'),
+	test = document.getElementById('test'),
 	message = document.getElementById('message'),
 	chineseReg = /[\u4e00-\ufa29]/g,
 	lenReg = /^.{4,16}$/;  //.查找单个字符，除了换行和行结束符
@@ -22,20 +23,18 @@ var msgs = {
 }
 
 function render(condition){
-	var name = document.getElementById('name');
 	message.innerHTML = condition.msg;
 	message.className = condition.spanClass;
-	name.className = condition.inputClass;
+	nameinput.className = condition.inputClass;
 }
 
 function testFun(){
-	var name = document.getElementById('name'),
-		value = name.value,
+	var val = nameinput.value,
 		condition;
-	value = value.replace(chineseReg,"--");  //用两个英文符号代替一个中文字符
-	if(value.length == 0){
+	val = val.replace(chineseReg,"--");  //用两个英文符号代替一个中文字符
+	if(val.length == 0){
 		condition = msgs.blank_msg;
-	}else if(!lenReg.test(value)){
+	}else if(!lenReg.test(val)){
 		condition = msgs.error_msg;
 	}else{
 		condition = msgs.right_msg;
